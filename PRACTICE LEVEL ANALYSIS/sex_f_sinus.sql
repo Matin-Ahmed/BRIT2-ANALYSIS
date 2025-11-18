8d768fdf32693563c0e494bafa4a34a6a8bf49d4
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-11-18 15:57:20 +0000

    sex_f_sinus.sql (Number of female adult patients with incident sinusitits during trial period)

---
+++ PRACTICE LEVEL ANALYSIS/sex_f_sinus.sql
@@ -0,0 +1,65 @@
+--------------------
+--  sex_f_sinus 
+-- trial period  
+-- Number of female adult patients with incident sinusitits during trial period
+--------------------
+
+WITH patient_ages AS (
+  SELECT
+    PK_Patient_ID,
+    med.FK_Patient_Link_ID,
+    OrganisationCode AS prac_code,
+    B.Name AS prac_name,
+    Region AS region,
+    DATEDIFF(YEAR, dob, '2024-05-01') 
+      - CASE 
+          WHEN MONTH(dob) > 5 OR (MONTH(dob) = 5 AND DAY(dob) > 1) THEN 1 
+          ELSE 0 
+        END AS age
+  FROM 
+      [BRIT].[Patient] A 
+      INNER JOIN [BRIT].[Reference_GP_Practice] B 
+      ON B.PK_Reference_GP_Practice_ID=A.FK_Reference_GP_Practice_ID 
+      INNER JOIN [BRIT].[patient_link] pl 
+      ON A.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
+      INNER JOIN BRIT.GP_Events med  
+      ON med.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
+      INNER JOIN BRIT.Reference_SnomedCT snomed
+      ON snomed.PK_Reference_SnomedCT_ID = med.FK_Reference_SnomedCT_ID
+  WHERE
+    med.EventDate BETWEEN CAST('2024-05-01 00:00:00' AS DATETIME) and CAST('2025-05-01 00:00:00' AS DATETIME) 
+    AND Sex = 'F'
+    AND snomed.ConceptID IN (
+'67832005',
+'91038008',
+'68272006',
+'431231008',
+'15805002',
+'77919000',
+'35923002',
+'73237007',
+'60130002',
+'88850006',
+'40055000',
+'897657000',
+'78737005',
+'88348008',
+'195790000',
+'195788001',
+'36971009'
+)
+),
+adult_patients AS (
+  SELECT DISTINCT PK_Patient_ID,prac_code,prac_name,region
+  FROM patient_ages
+  WHERE age >= 18
+)
+INSERT INTO PerPracticeData(prac_code,prac_name,region,sex_f_sinus)
+  SELECT
+    prac_code,
+    prac_name,
+    region,
+    COUNT(PK_Patient_ID) AS sex_f_sinus
+  FROM adult_patients
+  GROUP BY prac_code,prac_name,region
+  ORDER BY prac_code,prac_name,region;
