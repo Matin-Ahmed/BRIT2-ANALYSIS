f1d486cb980d84902f94c6228a1ba40df07c76a1
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-11-18 15:48:04 +0000

    sex_fem.sql (Number of female adult patients.)

---
+++ PRACTICE LEVEL ANALYSIS/sex_fem.sql
@@ -0,0 +1,39 @@
+--------------------
+-- sex_fem 
+-- Baseline  
+-- Number of female adult patients.
+--------------------
+
+WITH patient_ages AS (
+  SELECT
+    PK_Patient_ID,
+    OrganisationCode AS prac_code,
+    B.Name AS prac_name,
+    Region AS region,
+    DATEDIFF(YEAR, dob, '2024-05-01') 
+      - CASE 
+          WHEN MONTH(dob) > 5 OR (MONTH(dob) = 5 AND DAY(dob) > 1) THEN 1 
+          ELSE 0 
+        END AS age
+  FROM 
+      [BRIT].[Patient] A 
+      INNER JOIN [BRIT].[Reference_GP_Practice] B 
+      ON B.PK_Reference_GP_Practice_ID=A.FK_Reference_GP_Practice_ID 
+  WHERE 
+      Sex = 'F'
+),
+adult_patients AS (
+  SELECT DISTINCT PK_Patient_ID,prac_code,prac_name,region
+  FROM patient_ages
+  WHERE age >= 18
+)
+INSERT INTO PerPracticeData(prac_code,prac_name,region,sex_fem)
+  SELECT
+    prac_code,
+    prac_name,
+    region,
+    COUNT(PK_Patient_ID) AS sex_fem
+  FROM adult_patients
+  GROUP BY prac_code,prac_name,region
+  ORDER BY prac_code,prac_name,region
+;
