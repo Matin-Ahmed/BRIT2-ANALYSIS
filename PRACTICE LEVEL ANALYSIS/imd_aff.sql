fa1ff12ca74de3495a06b30da8bf72cc2f275f25
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-11-18 16:03:45 +0000

    imd_aff.sql (Number of adult patients from affluent areas)

---
+++ PRACTICE LEVEL ANALYSIS/imd_aff.sql
@@ -0,0 +1,26 @@
+--------------------
+--imd_aff Baseline  Number of adult patients from affluent areas  (Rankings from 19707 to 26275)
+--------------------
+WITH patient_ages AS (
+  SELECT
+    PK_Patient_ID,
+    OrganisationCode AS prac_code,
+    B.Name AS prac_name,
+    Region AS region,
+    IMD_Score,
+    DATEDIFF(YEAR, dob, '2024-05-01')
+      - CASE WHEN MONTH(dob) > 5 OR (MONTH(dob) = 5 AND DAY(dob) > 1) THEN 1 ELSE 0 END AS age
+  FROM [BRIT].[Patient] A INNER JOIN [BRIT].[Reference_GP_Practice] B ON B.PK_Reference_GP_Practice_ID=A.FK_Reference_GP_Practice_ID
+),
+adult_patients AS (
+  SELECT *
+  FROM patient_ages P
+  WHERE age >= 18 AND IMD_Score BETWEEN 19707 AND 26275
+)
+INSERT INTO PerPracticeData(prac_code,prac_name,region,imd_aff)
+  SELECT 
+    prac_code,prac_name,region,
+    COUNT(DISTINCT PK_Patient_ID) AS imd_aff
+  FROM adult_patients
+  GROUP BY prac_code,prac_name,region
+;
