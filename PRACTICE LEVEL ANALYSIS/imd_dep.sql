455ef799b95fd2638f4b071b5a199823a9db284c
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-11-18 16:02:21 +0000

    imd_dep.sql (Number of adult patients from deprived areas  (Rankings from 6569 to 13137)

---
+++ PRACTICE LEVEL ANALYSIS/imd_dep.sql
@@ -0,0 +1,28 @@
+--------------------
+-- imd_dep  
+-- Baseline 
+-- Number of adult patients from deprived areas  (Rankings from 6569 to 13137)
+--------------------
+WITH patient_ages AS (
+  SELECT
+    PK_Patient_ID,
+    OrganisationCode AS prac_code,
+    B.Name AS prac_name,
+    Region AS region,
+    IMD_Score,
+    DATEDIFF(YEAR, dob, '2024-05-01')
+      - CASE WHEN MONTH(dob) > 5 OR (MONTH(dob) = 5 AND DAY(dob) > 1) THEN 1 ELSE 0 END AS age
+  FROM [BRIT].[Patient] A INNER JOIN [BRIT].[Reference_GP_Practice] B ON B.PK_Reference_GP_Practice_ID=A.FK_Reference_GP_Practice_ID
+),
+adult_patients AS (
+  SELECT *
+  FROM patient_ages P
+  WHERE age >= 18 AND IMD_Score BETWEEN 6569 AND 13137
+)
+INSERT INTO PerPracticeData(prac_code,prac_name,region,imd_dep)
+  SELECT 
+    prac_code,prac_name,region,
+    COUNT(DISTINCT PK_Patient_ID) AS imd_dep
+  FROM adult_patients
+  GROUP BY prac_code,prac_name,region
+;
