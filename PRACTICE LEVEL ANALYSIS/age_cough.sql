8aabee4a432228e850255253880b862fcff8eb25
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-12-10 10:44:04 +0000

    age_cough.sql (Mean age of adult patients with incident cough during trial period)

--- PRACTICE LEVEL ANALYSIS/age_cough.sql
+++ PRACTICE LEVEL ANALYSIS/age_cough.sql
@@ -1,6 +1,6 @@
 --------------------
 -- age_cough 
--- Baseline  
+-- Age calculated at baseline, cough incident during trial  
 -- Mean age of adult patients with incident cough during trial period
 --------------------
 
@@ -27,7 +27,7 @@ WITH patient_ages AS (
       INNER JOIN BRIT.Reference_SnomedCT snomed
       ON snomed.PK_Reference_SnomedCT_ID = med.FK_Reference_SnomedCT_ID
   WHERE
-    med.EventDate BETWEEN CAST('2023-02-01 00:00:00' AS DATETIME) and CAST('2024-02-01 00:00:00' AS DATETIME) 
+    med.EventDate BETWEEN CAST('2024-05-01 00:00:00' AS DATETIME) and CAST('2025-05-01 00:00:00' AS DATETIME)
     AND snomed.ConceptID IN (
         '161929000',
         '28743005',
