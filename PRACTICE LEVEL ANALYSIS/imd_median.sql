b608b471e2a40f9b93bf25679b066bc6f9261cd3
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2025-12-10 10:38:18 +0000

    imd_median.sql (Median IMD ranking of adult patients.)

--- PRACTICE LEVEL ANALYSIS/imd_median.sql
+++ PRACTICE LEVEL ANALYSIS/imd_median.sql
@@ -17,7 +17,7 @@ WITH patient_ages AS (
 adult_patients AS (
   SELECT DISTINCT prac_code,prac_name,region,P.IMD_Score
   FROM patient_ages P
-  WHERE age >= 18 AND IMD_Score IS NOT NULL
+  WHERE age >= 18 AND IMD_Score IS NOT NULL AND IMD_Score > 0
 ),
 practice_medians AS (
   SELECT DISTINCT
