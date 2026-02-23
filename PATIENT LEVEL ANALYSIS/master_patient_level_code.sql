3abb7b4f69d9c3df6c548dcea294828a173d6709
Matin-Ahmed <matin.ahmed@manchester.ac.uk>
2026-02-23 17:17:12 +0000

    master_patient_level_code.sql (Master SQL script for generating the BRIT2 patient level analytical table and derived variables)

--- PATIENT LEVEL ANALYSIS/master_patient_level_code.sql
+++ PATIENT LEVEL ANALYSIS/master_patient_level_code.sql
@@ -1011,7 +1011,7 @@ INNER JOIN flu_flag ff
         ON pat.PK_Patient_ID = P.Patient_ID
     INNER JOIN [BRIT].[patient_link] pl
         ON pat.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
-    INNER JOIN BRIT.GP_Medications_AB_Clean med
+    INNER JOIN BRIT.GP_Medications med
         ON med.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
     WHERE
         med.MedicationDate >= DATEADD(YEAR, -1, P.event_date)
@@ -8236,7 +8236,7 @@ INNER JOIN ab_counts ac
         ON pat.PK_Patient_ID = P.Patient_ID
     INNER JOIN [BRIT].[patient_link] pl
         ON pat.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
-    INNER JOIN BRIT.GP_Medications_AB_Clean med
+    INNER JOIN BRIT.GP_Medications med
         ON med.FK_Patient_Link_ID = pl.PK_Patient_Link_ID
     WHERE
         CAST(med.MedicationDate AS DATE) = P.event_date
